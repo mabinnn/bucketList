a36d2607bb3aaf91cb433c6a616de5d15242aa33
Marvin Alganes <marvin.alganes@gmail.com>
2017-05-23 11:08:38 -0700

    first commit

---
+++ CancelDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  CancelDelegate.swift
+//  BucketListNew
+//
+//  Created by Marvin Alganes on 5/22/17.
+//  Copyright © 2017 Marvin Alganes. All rights reserved.
+//
+
+import Foundation
---
+++ SaveDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SaveDelegate.swift
+//  BucketListNew
+//
+//  Created by Marvin Alganes on 5/22/17.
+//  Copyright © 2017 Marvin Alganes. All rights reserved.
+//
+
+import Foundation
